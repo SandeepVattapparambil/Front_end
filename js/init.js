280e2dae6cac63cf60ebb584cbea0531406e52a9
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-06-27 16:55:48 +0530

    removal of files , added scroll button

--- js/init.js
+++ js/init.js
@@ -1,8 +1,25 @@
+/****************Materialize Initialization***************************/
 (function($){
   $(function(){
-
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-	
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+/******************Scroll to top initialization************************/
+jQuery(document).ready(function() {
+    var offset = 220;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+    
+    jQuery('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
--- js/nicescroll_customization.js
+++ js/nicescroll_customization.js
@@ -5,10 +5,3 @@ $(document).ready(function() {
 			zindex: "999"
 			});
 });
-$(document).ready(function() {  
-        $(".side-nav").niceScroll({
-			cursorcolor:"#666",
-			cursorborder: "1px solid #666",
-			zindex: "999"
-			});
-});
