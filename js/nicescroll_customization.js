7a3e937e5412e1849ec9036721eb7921338b0e8b
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-06-27 16:24:09 +0530

    nicescroll added

---
+++ js/nicescroll_customization.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {  
+        $("html").niceScroll({
+			cursorcolor:"#666",
+			cursorborder: "1px solid #666",
+			zindex: "999"
+			});
+});
+$(document).ready(function() {  
+        $(".side-nav").niceScroll({
+			cursorcolor:"#666",
+			cursorborder: "1px solid #666",
+			zindex: "999"
+			});
+});
