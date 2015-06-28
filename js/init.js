5ed62820597b2a324b9c1c27ac2317c004c3995f
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-06-28 21:48:55 +0530

    scroll to div

--- js/init.js
+++ js/init.js
@@ -51,4 +51,9 @@ $(window).scroll(function() {
 	   preloader.off();
    }
 });
-/*****************************************************/
+/*****************scroll to div**********************************/
+jQuery('#down').click(function(e) {
+    jQuery('html,body').animate({ scrollTop: jQuery(this.hash).offset().top}, 1000);
+    return false;
+    e.preventDefault();
+ });
