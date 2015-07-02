9b56b73d8a3c57cac93d31374fda51a5529c4046
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-07-02 21:48:47 +0530

    form edit

--- process/form.php
+++ process/form.php
@@ -1,33 +1,44 @@
 <?php
-/*if (($_SERVER['REQUEST_METHOD'] == 'POST') && (!empty($_POST['action']))):
+if (($_SERVER['REQUEST_METHOD'] == 'POST') && (!empty($_POST['action'])))
 
-	if (isset($_POST['name'])) { $name = $_POST['name']; }
-	if (isset($_POST['email'])) { $email = $_POST['email']; }
-	if (isset($_POST['message'])) {
-			$message = filter_var($_POST['message'], FILTER_SANITIZE_STRING ); 
+	if (isset($_POST['name'])) 
+	{ 
+	$name = $_POST['name']; 
 	}
-	$file = fopen("test.txt","w");
-		echo fwrite($name, $email, $message);
-		fclose($file);
-	// use wordwrap() if lines are longer than 70 characters
-	$msg = wordwrap($message,70);
-  $formdata = array (
-    'name' => $name,
-    'email' => $email,
-    'message' => $msg
-  );
-		$to			= 		"sandeepv68@gmail.com";
-		$subject	=		"Message from $name";
-		$messages	=		json_encode($formdata);
-		$headers = 'From: www.codelarva.com' . "\r\n" .
-					'Reply-To: codelarav@gmail.com' . "\r\n" .
-					'X-Mailer: PHP/' . phpversion();
-		
-		
-		mail($to, $subject, $messages, $headers);
+	if (isset($_POST['email'])) 
+	{ 
+	$email = $_POST['email']; 
+	}
+	if (isset($_POST['message'])) 
+	{
+			$message = filter_var($_POST['message'], FILTER_SANITIZE_STRING ); 
+	}	
 
-endif; //form submitted
-header("Location:../index.html");//if success*/
+require '../PHPMailerAutoload.php';
 
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+//Set who the message is to be sent from
+$mail->setFrom($name);
+//Set an alternative reply-to address
+$mail->addReplyTo('noreply@codelarva.com', 'CodeLarva Technologies');
+//Set who the message is to be sent to
+$mail->addAddress('sandeepv68@gmail.com', 'Sandep Vattapparambil');
+//Set the subject line
+$mail->Subject = 'Message by' .$name. 'CodeLarva.com';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+//$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+$mail->msgHTML($message);
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+//Attach an image file
+//$mail->addAttachment('images/phpmailer_mini.png');
 
+//send the message, check for errors
+if (!$mail->send()) {
+    header("location:../index.html?q=0");//echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    header("location:../index.html");//echo "Message sent!";
+}
 ?>
