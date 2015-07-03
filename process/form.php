f1779bd4c1760c59c238f0f6f894710de8394622
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-07-03 20:06:17 +0530

    new form process

--- process/form.php
+++ process/form.php
@@ -1,3 +1,32 @@
 <?php
+if (($_SERVER['REQUEST_METHOD'] == 'POST') && (!empty($_POST['action'])))
+{
+	if (isset($_POST['name'])) 
+	{ 
+	$name = $_POST['name']; 
+	}
+	if (isset($_POST['email'])) 
+	{ 
+	$email = $_POST['email']; 
+	}
+	if (isset($_POST['message'])) 
+	{
+	$message = filter_var($_POST['message'], FILTER_SANITIZE_STRING ); 
+	}
+	$to			= 		"sandeepv68@gmail.com";
+	$subject	=		"Message by" .$name. "from CodeLarva Technologies";
+	$messages	=		$message;
 
+	$headers	=		"From: visitor@codelarva.com \r\n".
+								"Reply-To: do-not-reply@codelarva.com \r\n";
+
+	if(mail($to, $subject, $messages, $headers))
+	{
+		header("Location:../index.php?g=1");//if success
+	}
+	else
+	{
+		header("Location:../index.php?g=0");//if failed
+	}
+}
 ?>
