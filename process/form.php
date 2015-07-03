ca5979f2f893fb53492336c9d5d390bbe2b745ad
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-07-03 20:23:42 +0530

    email

--- process/form.php
+++ process/form.php
@@ -1,32 +1,25 @@
 <?php
-if (($_SERVER['REQUEST_METHOD'] == 'POST') && (!empty($_POST['action'])))
-{
-	if (isset($_POST['name'])) 
+//if (($_SERVER['REQUEST_METHOD'] == 'POST') && (!empty($_POST['action'])))
+//{
+	if (isset($_POST['names'])) 
 	{ 
-	$name = $_POST['name']; 
+	$name = $_POST['names']; 
 	}
 	if (isset($_POST['email'])) 
 	{ 
 	$email = $_POST['email']; 
 	}
-	if (isset($_POST['message'])) 
+	if (isset($_POST['textarea1'])) 
 	{
-	$message = filter_var($_POST['message'], FILTER_SANITIZE_STRING ); 
+	$message = filter_var($_POST['textarea1']); 
 	}
 	$to			= 		"sandeepv68@gmail.com";
 	$subject	=		"Message by" .$name. "from CodeLarva Technologies";
 	$messages	=		$message;
 
 	$headers	=		"From: visitor@codelarva.com \r\n".
-								"Reply-To: do-not-reply@codelarva.com \r\n";
-
-	if(mail($to, $subject, $messages, $headers))
-	{
-		header("Location:../index.php?g=1");//if success
-	}
-	else
-	{
-		header("Location:../index.php?g=0");//if failed
-	}
-}
+						"Reply-To: do-not-reply@codelarva.com \r\n";
+mail($to, $subject, $messages);
+header("Location:../index.php");//if success	
+//}
 ?>
