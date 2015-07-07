0663d480a666c52276d3e105e277b0c975d5c24d
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-07-07 19:19:42 +0530

    mail test

--- test.php
+++ test.php
@@ -1,3 +1,11 @@
 <?php
-phpinfo();
+$to      = 'sandeepv68@gmail.com';
+$subject = 'Test ';
+$message = 'hello This is a test message ';
+$headers = 'From: sandeep@codelarva.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+//phpinfo();
 ?>
