ead8ef3a489c5808f5a04c70ac9b200fb31913c6
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-07-07 18:18:23 +0530

    converted to php app

--- process/form.php
+++ process/form.php
@@ -5,10 +5,10 @@
     if(mail($toEmail, $subject, $_POST["content"], $mailHeaders)) 
 	{
         //print "<p class='success'>Mail Sent.</p>";
-		header("location:../index.html");
+		header("location:../index.php");
     } else 
 	{
         //print "<p class='Error'>Problem in Sending Mail.</p>";
-		header("location:../index.html");
+		header("location:../index.php");
     }
 ?>
