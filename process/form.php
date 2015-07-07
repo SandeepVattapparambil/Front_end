a2e7bd8f55c568fc0f9f1a5212e428689d5ada30
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-07-07 21:02:27 +0530

    from edits

--- process/form.php
+++ process/form.php
@@ -1,5 +1,5 @@
 <?php
-   /* $toEmail = "sandeepv68@gmail.com";
+ $toEmail = "sandeepv68@gmail.com";
     $mailHeaders = "From:codelarva@codelarva.com";
 	$subject = "Message by ". $_POST["userName"] ." from CodeLarva.com";
     if(mail($toEmail, $subject, $_POST["content"], $mailHeaders)) 
@@ -10,32 +10,5 @@
 	{
         //print "<p class='Error'>Problem in Sending Mail.</p>";
 		header("location:../index.php");
-    }
-/*/	
-$name = $_POST["userName"];	
-$email = $_POST["userEmail"];
-$message = $_POST["content"];
-
-$mail = new PHPMailer(); // create a new object
-$mail->IsSMTP(); // enable SMTP
-$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-$mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 465; // or 587
-$mail->IsHTML(true);
-$mail->Username = "email@gmail.com";
-$mail->Password = "password";
-$mail->SetFrom("example@gmail.com");
-$mail->Subject = "Test";
-$mail->Body = "hello";
-$mail->AddAddress("email@gmail.com");
- if(!$mail->Send())
-    {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-    }
-    else
-    {
-    echo "Message has been sent";
     }
 ?>
